c643020003b5d10cef8c9e542c3f2f1883a9b575
SeokJun Hong <hong921122@gmail.com>
2017-01-04 14:39:50 +0900

    sample

--- Assets/LoggingSystem/TestScript.cs
+++ Assets/LoggingSystem/TestScript.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 
 public class TestScript : MonoBehaviour {
+    public GameObject testObject;
 
 	// Use this for initialization
 	void Start () {
         //LogManager.Instance.SelectedLogFile("aa").LogFileFullPath
         //LogManager.Instance.CreateLogFile();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        LogManager.Instance.SelectedLogFile("aa").WriteLog("\t", "test111", "test");
-        LogManager.Instance.SelectedLogFile("bb").WriteLog("\t", "test222", "test333");
+        //CommandManager.Instance.AddCommand(ref testObject, typeof(Vector3));
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //LogManager.Instance.SelectedLogFile("aa").WriteLog("\t", "test111", "test");
+        //LogManager.Instance.SelectedLogFile("bb").WriteLog("\t", "test222", "test333");
         //LogManager.Instance.WriteLog("\t", "test", "test");
+        if (Input.GetMouseButton(0))
+        {
+            //CommandManager.Instance.ControlCommand(1.4f, "test");
+            
+            //CommandManager.Instance.SetValueRecursive(testObject, "transform.localScale.x", 140f);
+        }
     }
 }
